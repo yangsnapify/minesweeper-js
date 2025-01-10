998abf135a8f81133fb126ddb35f2dedc57ac4f3
jlgy <trustour9@gmail.com>
2025-01-10 23:09:35 +0800

    add mapItemsType & formatMapItems()

--- app.js
+++ app.js
@@ -2,15 +2,37 @@
     
     class MineSweeper {
         constructor() {
-            this.map = null;
+            this.maps = null;
+            this.mapItemType = {
+                0 : "MINES",
+                1 : "DEFAULT"
+            }
         }
 
         createMap(x) {
-            if (!this.map) {
-                this.map = Array.from({ length: x }, () => Array.from({ length: x }, () => null));
-                console.log(this.map)
+            if (!this.maps) {
+                this.maps = Array.from({ length: x }, () => Array.from({ length: x }, () => null));
             }
-            return this.map;
+            return this.maps;
+        }
+
+        formatMapItems(x, y) {
+            let minesCount = 0;
+            const directions = [
+                [-1, 0], [1, 0], [0, -1], [0, 1],
+                [-1, -1], [-1, 1], [1, -1], [1, 1]
+            ]
+            directions.forEach(([dx, dy]) => {
+                const nx = dx + x;
+                const ny = dy + y;
+
+                if (nx >= 0 && nx < this.maps.length && ny >= 0 && ny < this.maps.length ) {
+                    if (this.maps[nx][ny] === this.mapType[0]) {
+                        minesCount += 1
+                    }
+                }
+            })
+            return minesCount;
         }
     }
 
