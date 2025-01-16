d28685a590536946573b616a16333309af09909c
jlgy <trustour9@gmail.com>
2025-01-16 22:40:07 +0800

    add updateStatusMines() & countEl

--- app.js
+++ app.js
@@ -9,6 +9,52 @@
             this.selectMode = this.mode[0];
             this.maxFillDepth = 3;
             this.minesCount = 0;
+            this.countEl = null;
+            this.mapEl = null;
+            this.markMinesArr = [];
+            this.minesCoords = []
+        }
+
+        setCountEl(el) {
+            const _el = document.getElementById(el);
+            if (_el) {
+                this.countEl = _el;
+            }
+        }
+        reRunCount() {
+            this.countEl.innerHTML = this.minesCount;
+        }
+
+        updateMinesStatus(x, y, el) {
+            x = parseInt(x);
+            y = parseInt(y);
+
+            if (this.minesCount === 0 && this.minesCoords.every((j, i) => j[0] === this.markMinesArr[i][0] && j[1] === this.markMinesArr[i][1] )) {
+                console.log('win')
+                this.run()
+                return;
+            }
+
+            const isMarked = this.markMinesArr.some(j => j[0] === x && j[1] === y);
+          
+            if (el) {
+                if (isMarked) {
+                    this.markMinesArr = this.markMinesArr.filter((j, i) => j[0] + j[1] !== x + y);
+                    el.style.background = "none"
+                    this.minesCount++;
+                    this.reRunCount();
+                    this.markReveal(x, y, false);
+                } else {
+                    if (this.minesCount === 0) {
+                        return;
+                    }
+                    this.markMinesArr.push([x, y]);
+                    el.style.background = "red"
+                    this.minesCount--;
+                    this.reRunCount();
+                    this.markReveal(x, y);
+                }
+            }
         }
 
         fmtData(x, y, v, r = false) {
@@ -22,7 +68,8 @@
 
         run() {
             this.createMap();
-            this.placeMines(this.getMinesPercentage()).readyGrid()
+            this.placeMines(this.getMinesPercentage()).readyGrid();
+            this.reRunCount();
         }
 
         createMap() {
@@ -63,16 +110,18 @@
 
         formatMapItems(x, y) {
             let that = this;
-            if (this.grid[x][y].v === this.mapItemType[0]) return this.fmtData(x, y, this.mapItemType[0]);
+            if (this.grid[x][y].v === this.mapItemType[0]) {
+                return this.fmtData(x, y, this.mapItemType[0]);
+            }
 
-            let minesCount = 0;
+            let _minesCount = 0;
             this.checkNeighbor(x, y, function (nx, ny) {
                 if (!that.checkIsBound(nx, ny)) return;
                 if (that.grid[nx][ny].v === that.mapItemType[0]) {
-                    minesCount += 1
+                    _minesCount += 1
                 }
             })
-            return this.fmtData(x, y, minesCount);
+            return this.fmtData(x, y, _minesCount);
         }
 
         readyGrid() {
@@ -90,19 +139,13 @@
             while (current < mines) {
                 const x = Math.floor(Math.random() * this.gridSize);
                 const y = Math.floor(Math.random() * this.gridSize);
-
+                this.minesCoords.push([x, y]);
                 if (this.grid[x][y].v !== this.mapItemType[0]) {
                     this.grid[x][y] = this.fmtData(x, y, this.mapItemType[0])
                     current += 1;
                 }
             }
             return this;
-        }
-
-        markMines(x, y, isRevealed) {
-            if (!isRevealed) {
-                
-            }
         }
 
         getMinesPercentage() {
@@ -120,10 +163,10 @@
             return Math.round(totalGridSize * minesPercentage);
         }
 
-        markReveal(x, y) {
+        markReveal(x, y, v) {
             this.grid[x][y] = {
                 ...this.grid[x][y],
-                revealed: true
+                revealed: typeof v === "boolean" ? v : true
             }
         }
 
